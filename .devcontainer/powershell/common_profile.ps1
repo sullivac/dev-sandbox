2f04d3cf793a40a818f360f835126e1268b5d74f
Corey Sullivan <sullivac@fastmail.fm>
2020-03-08 01:48:38 -0600

    Add kubectl.

--- .devcontainer/powershell/common_profile.ps1
+++ .devcontainer/powershell/common_profile.ps1
@@ -4,3 +4,12 @@ Import-Module -Name posh-git
 $env:PATH = "${env:PATH}:/root/.nvm/versions/node/v12.16.0/bin"
 
 Set-PSReadLineOption -EditMode Windows
+
+if ($env:SYNC_LOCALHOST_KUBECONFIG) {
+    $kubePath = $HOME | Join-Path -ChildPath .kube
+    New-Item -ItemType Directory -Path $kubePath -Force | Out-Null
+    $kubeLocalhostSourcePath = $HOME | Join-Path -ChildPath .kube-localhost -AdditionalChildPath *
+    Copy-Item -Recurse -Path $kubeLocalhostSourcePath -Destination $kubePath -Force
+    $kubeConfigPath = $kubePath | Join-Path -ChildPath config
+    sed -i -e "s/localhost/host.docker.internal/g" $kubeConfigPath
+}
