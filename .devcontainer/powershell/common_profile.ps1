f5c39f66c75e678b4adfb635d3d2f3cfcf5a489a
Corey Sullivan <sullivac@fastmail.fm>
2021-03-06 09:38:20 -0600

    Update Node.js to 14.16.0.

--- .devcontainer/powershell/common_profile.ps1
+++ .devcontainer/powershell/common_profile.ps1
@@ -4,7 +4,7 @@ Import-Module -Name posh-git
 $env:JAVA_HOME = "/usr/lib/jvm/java-11-openjdk-amd64"
 
 $paths = @(
-    '/root/.nvm/versions/node/v12.18.1/bin',
+    '/root/.nvm/versions/node/v14.16.0/bin',
     '/opt/gradle/gradle-6.8.3/bin'
 ) -join ':'
 
